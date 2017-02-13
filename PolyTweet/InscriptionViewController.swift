e2ff0e6ac84378ba5f1d6997e60137d0e6a53827
zambran <zambrano.nicolas@outlook.com>
2017-02-13 13:29:02 +0100

    add select in inscription

--- PolyTweet/InscriptionViewController.swift
+++ PolyTweet/InscriptionViewController.swift
@@ -8,16 +8,47 @@
 
 import Foundation
 import UIKit
-class InscriptionViewController: UIViewController {
+import CoreData
+class InscriptionViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return departement.count;
+    }
+
     var nom : String = "";
-    
+    var departement : [Departement] = [];
     
     @IBOutlet weak var username: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return
+        }
+        let context=appDelegate.persistentContainer.viewContext
+        let request : NSFetchRequest<Departement> = Departement.fetchRequest();
+        do{
+           try departement = context.fetch(request)
+        }
+        catch let error as NSError{
+            print(error);
+        }
+            let pickerView = UIPickerView()
+            
+            pickerView.delegate = self
+            
         
+            // Do any additional setup after loading the view, typically from a nib.
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return departement.count;
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return departement[row].name;
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
